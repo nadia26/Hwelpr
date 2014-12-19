1e9343ab85acff5c7acdb19abe50fb0b394a6f34
Claire Burghard <claireburghard@gmail.com>
2014-12-19 13:13:08 -0500

    Set up python and templates

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, flash, render_template, request, redirect, url_for, session, escape
+import api
+import mongo
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+@app.route("/home", methods=["GET","POST"])
+def home():
+    return render_template("home.html")
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()
+    
