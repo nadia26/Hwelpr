a3e049b8a73be9b68df4c2940775582223f4564d
Claire Burghard <claireburghard@gmail.com>
2014-12-29 15:44:25 -0500

    playing around with bootstrap, not complete

--- app.py
+++ app.py
@@ -16,25 +16,25 @@ def home():
             password = request.form["logpassword"]
             #this is where all the authentication mongo stuff would go 
             return redirect(url_for('welcome'))
-            if request.form['b']=="Sign Up":
-                username = request.form["signusername"]
-                password = request.form["signpassword"]
-                password2 = request.form["signpassword2"]
-                name = request.form["name"]
-                #this is where all the adding user mongo stuff would go 
-                message = "Registration Sucessful! Log In to get started." 
-                return render_template("home.html", message=message)
-            if request.form['b']=="Cancel":
-                return render_template("home.html", message=message)
+        if request.form['b']=="Sign Up":
+            username = request.form["signusername"]
+            password = request.form["signpassword"]
+            password2 = request.form["signpassword2"]
+            name = request.form["name"]
+            #this is where all the adding user mongo stuff would go 
+            message = "Registration Sucessful! Log In to get started." 
+            return render_template("home.html", message=message)
+        if request.form['b']=="Cancel":
+            return render_template("home.html", message=message)
     
-@app.route("/welcome",methods=["GET","POST"])
+@app.route("/welcome", methods=["GET","POST"])
 def welcome():
     if request.method=="GET":
         return render_template("welcome.html")
     else:
         #there will be other buttons here
         if request.form['b']=="Log Out":
-            return redirect(url_for('logout'))
+            return render_template("home.html")
 
 if __name__=="__main__":
     app.debug=True
