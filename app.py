6a8c19623aebab386015438d977f6bb53eb05262
Fish Milnikiewicz <fmilniki@gmail.com>
2014-12-31 13:38:20 -0500

    basic mongo stuff works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, render_template, request, redirect, url_for, session, escape
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.secret_key = 'secret'
@@ -14,19 +15,31 @@ def home():
         if request.form['b']=="Log In":
             username = request.form["logusername"]
             password = request.form["logpassword"]
-            #this is where all the authentication mongo stuff would go 
-            return redirect(url_for('welcome'))
-            if request.form['b']=="Sign Up":
-                username = request.form["signusername"]
-                password = request.form["signpassword"]
-                password2 = request.form["signpassword2"]
-                name = request.form["name"]
-                #this is where all the adding user mongo stuff would go 
-                message = "Registration Sucessful! Log In to get started." 
-                return render_template("home.html", message=message)
-            if request.form['b']=="Cancel":
-                return render_template("home.html", message=message)
-    
+            #mongo authentication stuff:
+            valid_user = authenticate(username,password)
+            if not(valid_user) and not(username==None):
+                return render_template("home.html",message="Not a valid user.")
+            else:
+                session['myuser']=username
+                return redirect(url_for('welcome'))
+        if request.form['b']=="Sign Up":
+            username = request.form["signusername"]
+            password = request.form["signpassword"]
+            password2 = request.form["signpassword2"]
+            name = request.form["name"]
+            #mongo adding user stuff:
+            if (len(username)<3 or len(password)<3):
+                return render_template("home.html",message="Please fill in required elements. Each required element must have at least 3 characters.")
+            elif(password == password2):
+                if adduser(username,password):
+                    return render_template("home.html", message="You have successfully registered.")
+                else:
+                    return render_template("home.html", message="Username taken. Try Again.")
+            else:
+                return render_template("home.html", message = "Password doesn't match confirmation.")
+        if request.form['b']=="Cancel":
+            return render_template("home.html", message=message)
+
 @app.route("/welcome",methods=["GET","POST"])
 def welcome():
     if request.method=="GET":
@@ -34,10 +47,33 @@ def welcome():
     else:
         #there will be other buttons here
         if request.form['b']=="Log Out":
-            return redirect(url_for('logout'))
+            session.pop("myuser", None)
+            return redirect(url_for('home'))
+
+def getpword(uname):
+    names = db.info.find()
+    for name in names:
+        if name['user'] == uname:
+            return name['pass']
+
+def authenticate(uname,pword):
+    names = db.info.find()
+    for name in names:
+        if name['user'] == uname:
+            if name['pass'] == pword:
+                return True
+    return False
+
+def adduser(uname,pword):
+    if db.info.find_one({'user':uname}) == None:
+        d = {'user':uname,'pass':pword}
+        db.info.insert(d)
+        return True
+    return False
 
 if __name__=="__main__":
+    client = MongoClient()
+    db = client['1258']
     app.debug=True
     app.run()
-
         
